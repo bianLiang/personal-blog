26e45050a33471958bd8fc462ae4b335847e6043
卞亮 <liangliang.bian@xrewin.com>
2018-08-14 17:37:48 +0800

    添加旋转动画

---
+++ js/home.js
@@ -0,0 +1,6 @@
+$(function(){
+    var bodyHeight = $(document).height() + 'px';
+    var bodyWidth = $(document).width() + 'px';
+    $('.bodyBox').css('height', bodyHeight)
+    $('.bodyBox').css('width', bodyWidth)
+ });
